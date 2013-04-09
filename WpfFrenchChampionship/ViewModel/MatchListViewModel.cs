40e7663376a00685023dd8a5d0899ce5242121e4
Stephane Mora <arsendeur@gmail.com>
2013-04-09 19:26:15 +0200

    first commit

---
+++ WpfFrenchChampionship/ViewModel/MatchListViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SoccerRankingLib;
+using System.Collections.ObjectModel;
+
+namespace WpfFrenchChampionship.ViewModel
+{
+    class MatchListViewModel : ViewModel
+    {
+        private ObservableCollection<Match> matches = new ObservableCollection<Match>();
+
+        public ObservableCollection<Match> Matches
+        {
+            get { return this.matches; }
+        }
+
+        public MatchListViewModel(Ranking ranking)
+        {
+            throw new NotImplementedException();  
+        }
+
+        public void ranking_NewMatchRegistered(object sender, SoccerRankingLib.Ranking.MatchRegistrationEventArgs e)
+        {
+            throw new NotImplementedException();   
+        }
+
+
+    }
+
+    
+
+
+}
